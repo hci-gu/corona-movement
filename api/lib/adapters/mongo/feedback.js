855150fc8418a9b6090350a2b5dff5b92fd0cb6a
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-09-07 22:02:59 +0200

    add feedback route to api

---
+++ api/lib/adapters/mongo/feedback.js
@@ -0,0 +1,46 @@
+const moment = require('moment')
+const AWS = require('aws-sdk')
+const COLLECTION = 'feedback'
+const {
+  AWS_ACCESS_KEY_ID,
+  AWS_ACCESS_KEY_SECRET,
+  AWS_S3_BUCKET_NAME,
+  AWS_S3_BUCKET_REGION,
+} = process.env
+
+let collection
+AWS.config.update({ region: AWS_S3_BUCKET_REGION })
+
+const s3 = new AWS.S3({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_ACCESS_KEY_SECRET,
+  region: AWS_S3_BUCKET_REGION,
+  signatureVersion: 'v4',
+  //   useAccelerateEndpoint: true
+})
+
+module.exports = {
+  init: async (db) => {
+    await db.createCollection(COLLECTION)
+    collection = db.collection(COLLECTION)
+  },
+  collection,
+  uploadFeedback: async (options) => {
+    const fileName = `${moment().format('YYYY-DD-MMTHH-mm-SS')}.png`
+    const s3Params = {
+      Bucket: AWS_S3_BUCKET_NAME,
+      Key: `wfhmovement/${fileName}`,
+      Expires: 60 * 60,
+      ContentType: 'image/png',
+    }
+
+    await collection.insert({
+      image: `https://${AWS_S3_BUCKET_NAME}.s3.amazonaws.com/wfhmovement/${fileName}`,
+      ...options,
+    })
+
+    const url = await s3.getSignedUrl('putObject', s3Params)
+
+    return url
+  },
+}
