120ea1ce91468313bed1426105c90d0695a28a6f
Sebastian Andreasson <xandse@ml-200403-001.lan>
2020-10-16 00:50:00 +0200

    populate empty data for users with compareDate before historical data

--- api/lib/index.js
+++ api/lib/index.js
@@ -10,6 +10,11 @@ const moment = require('moment')
 
 const PORT = process.env.PORT ? process.env.PORT : 4000
 
+const wait = (time) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(), time)
+  })
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.json({ limit: '25mb' }))
@@ -55,6 +60,7 @@ app.post('/health-data', async (req, res) => {
   await db.saveSteps({ id, dataPoints })
 
   if (createAggregation) {
+    await wait(1000)
     await Promise.all([
       db.saveAggregatedSteps(id),
       db.saveAggregatedSummary(id),
