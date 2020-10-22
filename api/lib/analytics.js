d5bad5f70f1193626328f9594eadd7d57dfed839
Mattias Rost <rrostt@gmail.com>
2020-10-22 11:55:31 +0200

    Require auth token for dashboard.

--- api/lib/analytics.js
+++ api/lib/analytics.js
@@ -2,15 +2,23 @@ const express = require('express')
 const router = express.Router()
 const db = require('./adapters/db')
 
-router.get('/numUsers', (req, res) => {
+const reqToken = (req, res, next) => {
+  const token = req.headers['Authorization']
+  if (token !== process.env.DASHBOARD_AUTH_TOKEN) {
+    throw new Error('not authed')
+  }
+  next()
+}
+
+router.get('/numUsers', reqToken, (req, res) => {
   res.json(db.getAllUsers().length)
 })
 
-router.get('/totalSteps', (req, res) => {
+router.get('/totalSteps', reqToken, (req, res) => {
   res.json(db.getTotalSteps())
 })
 
-router.get('/userRegistrations', (req, res) => {
+router.get('/userRegistrations', reqToken, (req, res) => {
   res.json(db.getAllUsers().map(({ created }) => created))
 })
 
